4720dc79257012581203c3812442054ffe9a9d7e
Duyb2014733 <ldduy038@gmail.com>
2024-03-29 15:21:43 +0700

    Cai dat trang loi 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,9 @@
-import ContactBook from "@/views/ContactBook.vue";
 import { createRouter, createWebHistory } from "vue-router";
 const routes = [
   {
-    path: "/",
-    name: "contactbook",
-    component: ContactBook,
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
   },
 ];
 const router = createRouter({
